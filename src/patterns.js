a4be69acc576287275eb688c899b1d54e75dacf7
Timo Tijhof <krinklemail@gmail.com>
2016-06-23 16:47:42 +0200

    patterns: Move to a seperate file

---
+++ src/patterns.js
@@ -0,0 +1,55 @@
+module.exports = [
+	// Migration
+	{
+		regex: /^((?!.*function addOnloadHook\s*\().*)addOnloadHook\s*\(/g,
+		replacement: '$1$(',
+		summary: 'addOnloadHook is deprecated'
+	},
+	{
+		regex: /(?:window\.)?escapeQuotesHTML\s*\(/g,
+		replacement: 'mw.html.escape(',
+		summary: 'escapeQuotesHTML is deprecated'
+	},
+	{
+		regex: /mw\.util\.wikiGetlink\s*\(/g,
+		replacement: 'mw.util.getUrl(',
+		summary: 'mw.util.wikiGetlink is deprecated'
+	},
+	{
+		regex: /(?:window\.)?escapeQuotes\s*\(/g,
+		replacement: 'mw.html.escape(',
+		summary: 'escapeQuotes is deprecated'
+	},
+	{
+		regex: /\bimportScriptURI\s*\(/g,
+		replacement: 'mw.loader.load(',
+		summary: 'importScriptURI is deprecated'
+	},
+	{
+		regex: /\bimportStylesheetURI\s*\(\s*([^\n\)]+?)\s*\)/g,
+		replacement: 'mw.loader.load($1, \'text/css\')',
+		summary: 'importStylesheetURI is deprecated'
+	},
+	{
+		regex: 'http://toolserver.org/~kolossos/openlayers/kml-on-ol.php',
+		replacement: 'https://tools.wmflabs.org/wiwosm/osm-on-ol/kml-on-ol.php',
+		summary: 'Update old toolserver url'
+	},
+	// Clean-up (not worth an edit of itself)
+	{
+		regex: /[?&]s?maxage=\d*/g,
+		replacement: ''
+	},
+	{
+		regex: /[?&]dontcountme=s/g,
+		replacement: ''
+	},
+	{
+		regex: /\.org\/(\?title=.*&action=raw&ctype=)/g,
+		replacement: '.org/w/index.php$1'
+	},
+	{
+		regex: /\$j\s*(\(|\.)/g,
+		replacement: '$$$1'
+	}
+];
--- wmf-tour-bot.js
+++ wmf-tour-bot.js
@@ -1,4 +1,3 @@
-/*jshint node:true, esversion:6 */
 var readline = require('readline');
 var url = require('url');
 var util = require('util');
@@ -11,63 +10,9 @@ require('colors');
 
 var auth = require('./tour.mwapi.json');
 var results = require('fs').readFileSync('./results.txt').toString();
+var patterns = require('./src/patterns');
 var bots = Object.create(null);
 var dMap = null;
-var patterns = [
-	// Migration
-	{
-		regex: /^((?!.*function addOnloadHook\s*\().*)addOnloadHook\s*\(/g,
-		replacement: '$1$(',
-		summary: 'addOnloadHook is deprecated'
-	},
-	{
-		regex: /(?:window\.)?escapeQuotesHTML\s*\(/g,
-		replacement: 'mw.html.escape(',
-		summary: 'escapeQuotesHTML is deprecated'
-	},
-	{
-		regex: /mw\.util\.wikiGetlink\s*\(/g,
-		replacement: 'mw.util.getUrl(',
-		summary: 'mw.util.wikiGetlink is deprecated'
-	},
-	{
-		regex: /(?:window\.)?escapeQuotes\s*\(/g,
-		replacement: 'mw.html.escape(',
-		summary: 'escapeQuotes is deprecated'
-	},
-	{
-		regex: /\bimportScriptURI\s*\(/g,
-		replacement: 'mw.loader.load(',
-		summary: 'importScriptURI is deprecated'
-	},
-	{
-		regex: /\bimportStylesheetURI\s*\(\s*([^\n\)]+?)\s*\)/g,
-		replacement: 'mw.loader.load($1, \'text/css\')',
-		summary: 'importStylesheetURI is deprecated'
-	},
-	{
-		regex: 'http://toolserver.org/~kolossos/openlayers/kml-on-ol.php',
-		replacement: 'https://tools.wmflabs.org/wiwosm/osm-on-ol/kml-on-ol.php',
-		summary: 'Update old toolserver url'
-	},
-	// Clean-up (not worth an edit of itself)
-	{
-		regex: /[?&]s?maxage=\d*/g,
-		replacement: ''
-	},
-	{
-		regex: /[?&]dontcountme=s/g,
-		replacement: ''
-	},
-	{
-		regex: /\.org\/(\?title=.*&action=raw&ctype=)/g,
-		replacement: '.org/w/index.php$1'
-	},
-	{
-		regex: /\$j\s*(\(|\.)/g,
-		replacement: '$$$1'
-	}
-];
 
 function SkipFileError(message) {
 	this.name = 'SkipFileError';
