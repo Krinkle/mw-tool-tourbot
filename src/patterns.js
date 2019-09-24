8d3756d42c2484c9d797d76a4d832d22302a17c1
Timo Tijhof <krinklemail@gmail.com>
2019-09-24 17:31:52 +0100

    paterns: Remove swap for index.php query parameter order
    Very limited benefit, and fairly high risk of breakage, e.g.
    when strings are concatenated they would no longer apply to the
    'title' parameter if not noticed by the bot operator:
    
    Example of a bad diff:
    
     - "…/action=…&ctype=…&title=Foo/" + variable;
     + "…/title=Foo/&action=…&ctype=…" + variable;

--- src/patterns.js
+++ src/patterns.js
@@ -353,10 +353,6 @@ module.exports = [
     replacement: '.org/w/index.php$1'
   },
   {
-    regex: /\.org\/w\/index\.php\?action=raw&ctype=text\/(css|javascript)&title=([^'";]+)/,
-    replacement: '.org/w/index.php?title=$2&action=raw&ctype=text/$1'
-  },
-  {
     regex: /\$j\s*(\(|\.)/,
     replacement: '$$$1'
   },
