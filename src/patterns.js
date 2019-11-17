3d7a3d944a5a4c78c77b86ed39e23721b17f6fa4
Timo Tijhof <krinklemail@gmail.com>
2019-11-17 17:16:00 -0500

    patterns: Add swap to avoid mention of outdated module name in comments

--- src/patterns.js
+++ src/patterns.js
@@ -352,12 +352,6 @@ module.exports = [
     summary: 'Updated deprecated module name'
   },
   {
-    // Fix common typo in some copy-pasted file comments
-    regex: /, query\.ui\.progressbar, /,
-    replacement: ', ',
-    summary: 'Updated deprecated module name'
-  },
-  {
     // Replace old alias with new name (Gadget definition, and file comment)
     regex: /((?:@required modules:|dependencies=)[^|\]]*)(?:jquery\.ui\.core|jquery\.ui\.core\.styles|jquery\.ui\.accordion|jquery\.ui\.autocomplete|jquery\.ui\.button|jquery\.ui\.datepicker|jquery\.ui\.dialog|jquery\.ui\.draggable|jquery\.ui\.droppable|jquery\.ui\.menu|jquery\.ui\.mouse|jquery\.ui\.position|jquery\.ui\.progressbar|jquery\.ui\.resizable|jquery\.ui\.selectable|jquery\.ui\.slider|jquery\.ui\.sortable|jquery\.ui\.tabs|jquery\.ui\.tooltip|jquery\.ui\.widget|jquery\.effects\.core|jquery\.effects\.blind|jquery\.effects\.clip|jquery\.effects\.drop|jquery\.effects\.highlight|jquery\.effects\.scale|jquery\.effects\.shake)([\s,|\]])/,
     replacement: '$1jquery.ui$2',
@@ -367,6 +361,19 @@ module.exports = [
     // Remove duplicate of a new name (Gadget definition, and file comment)
     regex: /((?:@required modules:|dependencies=)[^|\]]*jquery\.ui\s*)(,\s*[^|,]+)?,\s*jquery\.ui([\s,|\]])/g,
     replacement: '$1$2$3',
+    summary: 'Updated deprecated module name'
+  },
+  {
+    // Fix common typo in some copy-pasted file comments
+    regex: /, query\.ui\.progressbar, /,
+    replacement: ', ',
+    summary: 'Updated deprecated module name'
+  },
+  {
+    // Update false-positive result in comments that refer to the functionality
+    // by using the (now-outdated) module name.
+    regex: /(\/\/.*jquery\.(?:effects|ui))\.([a-zA-Z.]+)/,
+    replacement: '$1 $2',
     summary: 'Updated deprecated module name'
   },
   {
